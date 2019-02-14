9e69a25a61842979dbb404fb6de578b7f12f8943
Zapolskiy Sergey <myliveweb@yandex.ru>
2019-02-14 09:22:30 +0300

    Create Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,5 @@
-@extends('layout')
+@extends('admin.layouts.app_admin')
+
 @section('content')
 <main role="main">
   <div class="album py-5 bg-light">
@@ -6,9 +7,7 @@
       <div class="row">
         <div class="col-md-12">
 		  <div class="alert alert-success" role="alert">
-			<h1>{{ $host->REMOTE_ADDR }}</h1>
-			<p>{{ $host->HTTP_USER_AGENT }}</p>
-      <p>{{ $host->created_at }} / {{ $host->updated_at }}</p>
+			<h1>Admin Dashboard</h1>
 		  </div>
         </div>
       </div>
