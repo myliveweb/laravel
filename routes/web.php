7976c4c735b5372e58e86be26db506feaac75a82
Zapolskiy Sergey <myliveweb@yandex.ru>
2019-02-14 07:43:34 +0300

    Add Auth

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/hosts', 'HostsController@index');
 Route::get('/hosts/{host}', 'HostsController@show');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
